a47d16d82a691b206341ade2cf7e03e453e95eb3
Vyalov Vadim <vyalov.vadim@gmail.com>
2023-06-19 18:43:33 +0300

    Fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,7 +40,7 @@ const logout = async (req, res) => {
 
 const current = async (req, res) => {
   const { email, subscription } = req.user;
-  res.status(200).json({ email, subscription });
+  res.status(200).json({ user: { email, subscription } });
 };
 
 const updateSubscription = async (req, res) => {
