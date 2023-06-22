f381b109e806be82d36f0c901a9ec9b3999d4ae0
Vyalov Vadim <vyalov.vadim@gmail.com>
2023-06-23 02:05:08 +0300

    Fix-avatar

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/userSchema");
 const { catchAsync, appError } = require("../utils");
 const path = require("path");
 const Jimp = require("jimp");
+const { unlink } = require("fs/promises");
 const { TOKEN } = require("../config/config");
 const { JWT_SECRET } = process.env;
 
@@ -9,7 +10,7 @@ const registration = catchAsync(async (req, res) => {
   const result = await User.create(req.body);
   const { email, subscription } = result;
 
-  res.status(201).json({ email, subscription });
+  res.status(201).json({ user: { email, subscription } });
 });
 
 const login = catchAsync(async (req, res) => {
@@ -48,7 +49,7 @@ const logout = async (req, res) => {
 
 const current = async (req, res) => {
   const { email, subscription } = req.user;
-  res.status(200).json({ user: { email, subscription } });
+  res.status(200).json({ email, subscription });
 };
 
 const updateSubscription = async (req, res) => {
@@ -65,16 +66,16 @@ const updateSubscription = async (req, res) => {
 const updateAvatar = async (req, res) => {
   const { filename } = req.file;
   const { id } = req.user;
-  const newFileName = `${id}_${filename}`;
 
   if (!filename) throw appError(401, "File is require!");
 
+  const newFileName = `${id}_${filename}`;
   const tmpPath = path.resolve(__dirname, "../tmp", filename);
-  const avatarsDir = path.resolve(__dirname, "../public/avatars", newFileName);
+  const avatarPath = path.resolve(__dirname, "../public/avatars", newFileName);
 
   const image = await Jimp.read(tmpPath);
-  await image.resize(250, 250).quality(60).writeAsync(avatarsDir);
-
+  await image.resize(250, 250).quality(60).writeAsync(avatarPath);
+  await unlink(tmpPath);
   const avatarURL = path.join("avatars", newFileName);
 
   await User.findByIdAndUpdate(id, { avatarURL }, { new: true });
